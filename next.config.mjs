3f6282ef1c51513606d7e222822f14790b534d4a
Tomás Escobar <escobartomas18@gmail.com>
2026-06-08 23:57:10 -0300

    Modify configs to deploy on vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  // Genera un build optimizado e independiente ideal para Vercel
+  output: 'standalone', 
+  // Si vas a cargar imágenes externas (como las del Bucket de Supabase), 
+  // debés habilitar el dominio de Supabase aquí
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
